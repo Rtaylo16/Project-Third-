2c99388f7c459695c42bddb4cad93a89cb67a934
Justin Taylor <kiradez211@gmail.com>
2020-06-22 20:24:47 -0400

    Added links routes

--- mediancol/src/App.js
+++ mediancol/src/App.js
@@ -7,35 +7,49 @@ import Footer from "./components/footer"
 //import  Background from "./components/images/CitiPicture.png";
 
 
-import {BrowserRouter as Router, Switch,Route,} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 export default function App() {
   return (
     <Router>
       <div className="main">
-      <Header />
-      <div  className="mainContent">
-        {/* A <Switch> looks through its children <Route>s and
+        <Header />
+        <div className="mainContent">
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/LivingCost">
-            <LivingCost />
-          </Route>
-          <Route path="/Home">
-            <Home/>
+
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/LivingCost">Living Cost</Link>
+              </li>
+            </ul>
+
+            <Switch>
+              <Route path="/LivingCost">
+                <LivingCost />
+              </Route>
+              <Route path="/Home">
+                <Home />
 
 
-           
-          </Route>
-          <Route path="/">
-            <Home />
-           
-          </Route>
-        </Switch>
+
+              </Route>
+              <Route path="/">
+                <Home />
+
+              </Route>
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer/>
-      </div>
     </Router>
   );
 }
+
