a19bc8561de80039c460cdd8ea14575e2f389c32
Justin Taylor <kiradez211@gmail.com>
2020-07-03 22:06:10 -0400

    authorization

---
+++ client/src/components/authenticate.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import PrivateRoute from './PrivateRoute';
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import { AuthContext } from "./context/auth";
+
+function Auth (props) {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home Page</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin Page</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Home} />
+        <Route path="/admin" component={Admin} />
+      </div>
+    </Router>
+  );
+}
+
+export default Auth;
